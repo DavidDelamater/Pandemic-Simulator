de4709a08e14217cba513f2bcc83f30d70f5fce6
DavidDelamater <David.W.Delamater@gmail.com>
2018-08-10 14:08:26 -0500

    Changed a lot of the game board, got lines somewhat working, need to fix up game board more.

--- src/GameWindow.py
+++ src/GameWindow.py
@@ -12,7 +12,10 @@ from PyQt5.QtWidgets import (
 from PyQt5.QtGui import (
     QIcon,
     QPixmap,
+    QPainter,
+    QPen,
 )
+from PyQt5.QtCore import Qt
 from src import GameConsts, GameUtils
 from src.GameSession import GameSession
 
@@ -28,22 +31,15 @@ class GameWindow(QWidget):
 
     def init_gui(self):
         # General window properties
-        self.setGeometry(50, 50, 1500, 800)
+        self.setMinimumSize(500, 500)
         self.setWindowTitle("Pandemic Simulator")
         self.setWindowIcon(QIcon("../icons/Pandemic_Icon.png"))
         self.main_hbox = QHBoxLayout()
-        self.window_size_lbl = QLabel("")
-        self.main_hbox.addWidget(self.window_size_lbl)
         self.setLayout(self.main_hbox)
         # Create the game board
-        self.game_board_frame = QFrame()
-        self.game_board_frame.setMinimumSize(850, 600)
-        self.game_board_frame.setFrameShape(QFrame.Panel)
-        self.game_board_frame.setFrameShadow(QFrame.Sunken)
+        self.game_board_frame = GameBoardFrame()
         self.main_hbox.addWidget(self.game_board_frame, 1)
         self.make_city_images()
-        self.draw_cities()
-
 
     def make_city_images(self):
         for city_name in self.game_session.cities:
@@ -60,7 +56,7 @@ class GameWindow(QWidget):
                 color_image = QPixmap("../icons/Pandemic_Icon.png")
             # Create the label
             image_label = QLabel()
-            image_label.setParent(self.game_board_frame)
+            image_label.setParent(self)
             image_label.setPixmap(color_image)
             self.city_images.append(image_label)
 
@@ -73,12 +69,9 @@ class GameWindow(QWidget):
         # Move the widgets
         for index, image in enumerate(self.city_images):
             coord_x, coord_y = coords[index]
-            coord_x = coord_x * self.game_board_frame.width()
-            coord_y = coord_y * self.game_board_frame.height()
+            coord_x = (coord_x * self.width()) - (image.width() / 2)
+            coord_y = (coord_y * self.height()) - (image.height() / 2)
             image.move(coord_x, coord_y)
-
-    def draw_lines(self):
-        pass
 
     def draw_cubes(self):
         pass
@@ -89,9 +82,66 @@ class GameWindow(QWidget):
     def draw_research_centers(self):
         pass
 
-    def draw_all(self):
-        self.draw_cities()
+    def draw_all(self, qp):
+        #self.draw_cities()
+        self.draw_lines(qp)
 
     def resizeEvent(self, QResizeEvent):
+        self.repaint()
+
+    def paintEvent(self, QPaintEvent):
         self.draw_all()
-        self.window_size_lbl.setText(str(self.game_board_frame.width()) + ", " + str(self.game_board_frame.height()))
+
+
+class GameBoardFrame(QFrame):
+    def __init__(self):
+        super(GameBoardFrame, self).__init__()
+        self.setMinimumSize(850, 600)
+        self.setFrameShape(QFrame.Panel)
+        self.setFrameShadow(QFrame.Sunken)
+
+    def draw_lines(self):
+        # Setup the painter
+        qp = QPainter()
+        qp.begin(self)
+        pen = QPen(Qt.black, 1, Qt.SolidLine)
+        qp.setPen(pen)
+        # Draw all of the lines
+        for city_name in GameConsts.city_names:
+            city_x_coord, city_y_coord = GameConsts.city_coords[city_name]
+            city_x_coord = city_x_coord * self.width()
+            city_y_coord = city_y_coord * self.height()
+            connected_cities = GameConsts.connected_cities[city_name]
+            for connected_city in connected_cities:
+                conn_city_x_coord, conn_city_y_coord = GameConsts.city_coords[connected_city]
+                conn_city_x_coord = conn_city_x_coord * self.width()
+                conn_city_y_coord = conn_city_y_coord * self.height()
+                qp.drawLine(city_x_coord, city_y_coord, conn_city_x_coord, conn_city_y_coord)
+        # Close the painter
+        qp.end()
+
+    def draw_cities(self):
+        # TODO: fix this
+        # Get the new coordinates
+        coords = []
+        for city_name in self.game_session.cities:
+            city = self.game_session.cities[city_name]
+            coords.append(city.coords)
+        # Move the widgets
+        for index, image in enumerate(self.city_images):
+            coord_x, coord_y = coords[index]
+            coord_x = (coord_x * self.width()) - (image.width() / 2)
+            coord_y = (coord_y * self.height()) - (image.height() / 2)
+            image.move(coord_x, coord_y)
+
+    def draw_cubes(self):
+        pass
+
+    def draw_pawns(self):
+        pass
+
+    def draw_all(self):
+        self.draw_lines()
+
+    def paintEvent(self, QPaintEvent):
+        self.draw_all()
